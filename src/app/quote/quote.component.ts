ecfd3b2682b5c9ae5ab9b913a9e9fd90272701ca
Samwelrangili <samwelrangili896@gmail.com>
2020-04-02 11:55:41 +0300

    initial commit

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote} from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
+    
+
+      if (toDelete){
+        this.quote.splice(index,1)
+      }
+else{
+  alert('Thank you for leaving your quote!!!')
+}
+    }
+  }
+  
+  quote: Quote[] = [
+    new Quote(1, "People Who Are Crazy Enough To Think They Can Change The World,Are One Who Do. Author-Rob Siltanen", "",new Date(2019,1,1)),
+    new Quote(2, "Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur. Author-Mohnish Pabrai","",new Date(2019,4,14)),
+    new Quote(3, "Imagine Your Life Is Perfect In Every Respect; What Would It Look Like?. Author-Brian Tracy","",new Date(2020,3,14)),
+    new Quote(4, "Security Is Mostly A Superstition. Life Is Either A Daring Adventure Or Nothing. Author-Helen Keller","",new Date(2018,5,24)),
+    new Quote(5, "What You Lack In Talent Can Be Made Up With Desire, Hustle And Giving 110% All The Time. Author-Don Zimmer","",new Date(2017,3,4)),
+  ];
+  toggleDetails(index){
+    this.quote[index].showDescription = !this.quote[index].showDescription;
+  }
+   
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quote.splice(index,1);
+    }
+  }
+  addNewQuote(quote){
+    let quoteLength = this.quote.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quote.push(quote)
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
